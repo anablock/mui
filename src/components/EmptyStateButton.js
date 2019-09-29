8147b4fe2771122e58041514e704b03d56d05a54
Vuk Djukic <vuchina@icloud.com>
2019-09-28 18:39:32 -0700

    Create EmptyStateButton.js

---
+++ src/components/EmptyStateButton.js
@@ -0,0 +1,15 @@
+<EmptyState
+              title={title}
+              description=""
+              button={
+                <Fab className = {classes.button}
+                  color = "primary"
+                  href = "https://github.com/VukDukic"
+                  rel = "noopener noreferrer"
+                  target = "_blank"
+                  variant = "extended" >
+                    <GitHubCircleIcon className={classes.buttonIcon} />
+                      Anablock
+                </Fab>
+              }
+            />
